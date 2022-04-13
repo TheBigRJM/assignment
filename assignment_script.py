edf409d7cd904375266f9b5524d39accf1275f05
ManOfAran <101205549+TheBigRJM@users.noreply.github.com>
2022-04-13 20:46:21 +0100

    Initial commit of assignment script

---
+++ assignment_script.py
@@ -0,0 +1,5 @@
+################################################
+## Code Written by Rory Middleton, April 2022 ##
+## As part of Ulster University egm722         ##
+## programming for GIS and Remote Sensing     ##
+
